45b208ec411b57f714c7ae6fbb086b0a532feeda
GuillaumeCisco <guillaumecisco@gmail.com>
2018-10-11 16:55:35 +0200

    Remove history

--- src/client/root/history/prod.js
+++
@@ -1,9 +0,0 @@
-/* globals PRODUCTION_BASE_NAME */
-
-import {createBrowserHistory} from 'history';
-
-export const history = createBrowserHistory({
-    basename: PRODUCTION_BASE_NAME,
-});
-
-export default history;
