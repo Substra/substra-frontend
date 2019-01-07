f3c3d237241c8a9a799474f1e70255df570238f9
GuillaumeCisco <guillaumecisco@gmail.com>
2019-01-07 15:08:07 +0100

    Support New ReactReduxContext with hot reloading

--- src/app/business/search/routes/web.js
+++ src/app/business/search/routes/web.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import universal from 'react-universal-component';
 import {injectSaga, injectReducer} from 'redux-sagas-injector';
 import {ReactReduxContext} from 'react-redux';
@@ -6,31 +6,45 @@ import {ReactReduxContext} from 'react-redux';
 import {PulseLoader} from 'react-spinners';
 import {coolBlue} from '../../../../../assets/css/variables/index';
 
-const Universal = () => {
-    const U = universal(import('../../search/components/index'), {
-        loading: <PulseLoader size={6} color={coolBlue}/>,
-        onLoad: (module, info, {reduxcontext, ...props}) => {
+class Universal extends Component {
+    constructor(props) {
+        super(props);
+        this.firstRender = true;
+    }
 
-            // need all models reducers
-            // do not forget to pass the reduxcontext.store AND the withRedux wrapper to your imported redux component, or concurrent calls in the server part will fail
-            injectSaga('challenge', module.challengeSagas, false, reduxcontext.store);
-            injectReducer('challenge', module.challengeReducer, false, reduxcontext.store);
+    render() {
+        const U = universal(import('../../search/components/index'), {
+            loading: <PulseLoader size={6} color={coolBlue}/>,
+            onLoad: (module, info, {reduxcontext, ...props}) => {
 
-            injectSaga('dataset', module.datasetSagas, false, reduxcontext.store);
-            injectReducer('dataset', module.datasetReducer, false, reduxcontext.store);
+                // need all models reducers
+                // do not forget to pass the reduxcontext.store AND the withRedux wrapper to your imported redux component, or concurrent calls in the server part will fail
 
-            injectSaga('algo', module.algoSagas, false, reduxcontext.store);
-            injectReducer('algo', module.algoReducer, false, reduxcontext.store);
+                if (reduxcontext && reduxcontext.store) {
+                    injectSaga('challenge', module.challengeSagas, false, reduxcontext.store);
+                    injectReducer('challenge', module.challengeReducer, false, reduxcontext.store);
 
-            injectSaga('model', module.modelSagas, false, reduxcontext.store);
-            injectReducer('model', module.modelReducer, false, reduxcontext.store);
-        },
-        ignoreBabelRename: true,
-    });
+                    injectSaga('dataset', module.datasetSagas, false, reduxcontext.store);
+                    injectReducer('dataset', module.datasetReducer, false, reduxcontext.store);
 
-    return <ReactReduxContext.Consumer>
-        {(reduxContext) => <U reduxcontext={reduxContext}/>}
-    </ReactReduxContext.Consumer>;
-};
+                    injectSaga('algo', module.algoSagas, false, reduxcontext.store);
+                    injectReducer('algo', module.algoReducer, false, reduxcontext.store);
+
+                    injectSaga('model', module.modelSagas, false, reduxcontext.store);
+                    injectReducer('model', module.modelReducer, false, reduxcontext.store);
+                }
+            },
+            ignoreBabelRename: true,
+        });
+        if (this.firstRender) {
+            this.firstRender = false;
+            return (<ReactReduxContext.Consumer>
+                {(reduxContext) => <U reduxcontext={reduxContext}/>}
+            </ReactReduxContext.Consumer>);
+        }
+
+        return <U/>;
+    }
+}
 
 export default Universal;
