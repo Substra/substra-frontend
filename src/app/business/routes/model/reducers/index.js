052731159cab440df5db3013c275fef8c7146d4c
Jérémy Morel <jeremy.morel@owkin.com>
2019-01-16 16:31:02 +0100

    sort: mutualise reducer

--- src/app/business/routes/model/reducers/index.js
+++ src/app/business/routes/model/reducers/index.js
@@ -1,12 +1,17 @@
 import list from '../../../common/reducers/list';
 import item from '../../../common/reducers/item';
 import persistent from '../../../common/reducers/persistent';
-import order from './order';
+import order from '../../../common/reducers/order';
 import {actionTypes} from '../actions';
+
+const orderInitialState = {
+    by: 'testData.perf',
+    direction: 'desc',
+};
 
 export default {
     list: list(actionTypes),
     item: item(actionTypes),
     persistent: persistent(actionTypes),
-    order: order(actionTypes),
+    order: order(actionTypes, orderInitialState),
 };
