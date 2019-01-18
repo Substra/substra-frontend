33ec21d83144ccfe0b07fc431fe0aef07d359771
Jérémy Morel <jeremy.morel@owkin.com>
2019-01-18 15:38:16 +0100

    sort: remove useless variable

--- src/app/business/routes/model/components/list/components/sort.js
+++ src/app/business/routes/model/components/list/components/sort.js
@@ -1,8 +1,6 @@
 import Sort from '../../../../../common/components/list/components/sort';
 
 export default class ModelSort extends Sort {
-    selectWidth = '140px';
-
     options = [
         {value: 'testData.perf-asc', label: 'LOWEST SCORE'},
         {value: 'testData.perf-desc', label: 'HIGHEST SCORE'},
