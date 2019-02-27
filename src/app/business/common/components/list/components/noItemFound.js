592dd9d508f2f3ce34d82c2ce6550bcdf819ad91
Jérémy Morel <jeremy.morel@owkin.com>
2019-02-27 18:22:50 +0100

    fix padding and wording for no result message

---
+++ src/app/business/common/components/list/components/noItemFound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import {spacingNormal, spacingSmall} from '../../../../../../../assets/css/variables/spacing';
+
+const Padding = styled('div')`
+    padding: ${spacingSmall} ${spacingNormal};
+`;
+
+const NoItemFound = ({model}) => (
+    <Padding>
+        {`No ${model} found`}
+    </Padding>
+);
+
+NoItemFound.propTypes = {
+    model: PropTypes.string.isRequired,
+};
+
+export default NoItemFound;
