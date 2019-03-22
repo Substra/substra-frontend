918343cf00ab965f962c5bd18ff1f2662b0df6ce
Jérémy Morel <jeremy.morel@owkin.com>
2019-03-22 09:12:14 +0100

    use dataset_manager route for dataset

--- src/app/business/routes/dataset/api.js
+++ src/app/business/routes/dataset/api.js
@@ -1,7 +1,7 @@
 import {fetchEntitiesFactory, fetchEntityFactory} from '../../../entities/fetchEntities';
 
-export const fetchListApi = fetchEntitiesFactory('dataset');
-export const fetchItemApi = fetchEntityFactory('dataset');
+export const fetchListApi = fetchEntitiesFactory('dataset_manager');
+export const fetchItemApi = fetchEntityFactory('dataset_manager');
 
 export default {
     fetchListApi,
