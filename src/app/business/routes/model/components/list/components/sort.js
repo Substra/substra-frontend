b91fc4c902ac3a0b3fb1c3f302e902d2a00e5d92
Jérémy Morel <jeremy.morel@owkin.com>
2019-04-24 12:14:10 +0200

    model bundles: sort by non certified score

--- src/app/business/routes/model/components/list/components/sort.js
+++ src/app/business/routes/model/components/list/components/sort.js
@@ -5,6 +5,8 @@ import Sort from '../../../../../common/components/list/components/sort/redux';
 const options = [
     {value: {by: 'testtuple.dataset.perf', direction: 'asc'}, label: 'LOWEST SCORE'},
     {value: {by: 'testtuple.dataset.perf', direction: 'desc'}, label: 'HIGHEST SCORE'},
+    {value: {by: 'nonCertifiedTesttuple.dataset.perf', direction: 'asc'}, label: 'LOWEST NON CERTIFIED SCORE'},
+    {value: {by: 'nonCertifiedTesttuple.dataset.perf', direction: 'desc'}, label: 'HIGHEST NON CERTIFIED SCORE'},
 ];
 
 export default props => <Sort {...props} options={options} />;
