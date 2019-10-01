6d5f43455641f73508a8e4ce6de7d08c4925432a
GuillaumeCisco <guillaumecisco@gmail.com>
2019-10-01 09:58:28 +0200

    Remove basic auth env variables

--- config/production.js
+++ config/production.js
@@ -1,9 +1,6 @@
 const encryption_privkey = '/etc/letsencrypt/live/substra-frontend.com/privkey.pem';
 const encryption_fullchain = '/etc/letsencrypt/live/substra-frontend.com/fullchain.pem';
 
-const name = process.env.FRONT_AUTH_USER;
-const pass = process.env.FRONT_AUTH_PASSWORD;
-
 const apiUrl = process.env.API_URL;
 
 module.exports = {
@@ -15,9 +12,5 @@ module.exports = {
     encryption: {
         privkey: encryption_privkey,
         fullchain: encryption_fullchain,
-    },
-    auth: {
-        name,
-        pass,
     },
 };
