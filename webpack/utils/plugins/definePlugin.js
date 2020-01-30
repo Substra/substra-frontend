ee07e9022920446fe50a0b710d6042ea6e892a44
Jérémy Morel <jeremy.morel@owkin.com>
2020-01-30 15:31:15 +0100

    only add API_URL to definePlugin for electron

--- webpack/utils/plugins/definePlugin.js
+++ webpack/utils/plugins/definePlugin.js
@@ -1,15 +1,19 @@
 import webpack from 'webpack';
 import config from 'config';
 
+const IS_ELECTRON = JSON.stringify(process.env.IS_ELECTRON || 'false');
+
 export default () => new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
     'process.env.DEBUG_PROD': JSON.stringify(process.env.DEBUG_PROD || 'false'),
-    'process.env.IS_ELECTRON': JSON.stringify(process.env.IS_ELECTRON || 'false'),
+    'process.env.IS_ELECTRON': IS_ELECTRON,
     'process.env.IS_STATIC': JSON.stringify(process.env.IS_STATIC || 'false'),
     APP_NAME: JSON.stringify(config.appName),
-    // API_URL: JSON.stringify(config.apps.frontend.apiUrl), // needed for electron
     META_DESCRIPTION: JSON.stringify(config.apps.frontend.meta.description),
     PRODUCTION_BASE_NAME: JSON.stringify(config.apps.frontend.baseName.production),
     DEBUG_BASE_NAME: JSON.stringify(config.apps.frontend.baseName.debug),
     SCORE_PRECISION: JSON.stringify(config.apps.frontend.scorePrecision),
+    ...(IS_ELECTRON !== 'false' ? {
+        API_URL: JSON.stringify(config.apps.frontend.apiUrl), // needed for electron
+    } : {}),
 });
