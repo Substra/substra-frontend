7e6e0c06308d95efe83d6e25d0c417047b19e692
Jérémy Morel <jeremy.morel@owkin.com>
2020-09-04 11:20:49 +0200

    remove unused code

--- src/app/entities/fetchEntities.js
+++ src/app/entities/fetchEntities.js
@@ -36,8 +36,6 @@ export const fetchList = (url, jwt) => {
         .then((json) => ({list: json, status}), (error) => ({error, status}));
 };
 
-export const fetchByUrl = (urlToFetch, jwt) => fetchList(urlToFetch, jwt);
-
 export const fetchEntitiesFactory = (path) => (get_parameters, jwt) => {
     const url = `${API_URL}/${path}/${!isEmpty(get_parameters) ? `?${queryString.stringify(get_parameters)}` : ''}`;
     return fetchList(url, jwt);
@@ -91,126 +89,6 @@ export const fetchEntityFactory = (path) => (get_parameters, id, jwt) => {
         .then((response) => {
             status = response.status;
             return handleResponse(response);
-        })
-        .then((json) => ({item: json, status}), (error) => ({error, status}));
-};
-
-export const deleteEntityFactory = (path) => (id, jwt) => {
-    const headers = getHeaders(jwt);
-    const url = `${API_URL}/${path}/${id}/`;
-
-    let status;
-
-    return fetch(url, {
-        method: 'DELETE',
-        headers,
-        // Allows API to set http-only cookies with AJAX calls
-        // @see http://www.redotheweb.com/2015/11/09/api-security.html
-        credentials: 'include',
-        mode: 'cors',
-    })
-        .then((response) => {
-            status = response.status;
-            if (status !== 204) {
-                return response.text().then((result) => Promise.reject(new Error(result)));
-            }
-
-            return response;
-        }).then(() => true, (error) => ({error, status}));
-};
-
-export const updateEntityFactory = (path) => (id, jwt, payload) => {
-    const headers = getHeaders(jwt);
-    const url = `${API_URL}/${path}/${id}/`;
-
-    return fetch(url, {
-        method: 'PATCH',
-        headers,
-        // Allows API to set http-only cookies with AJAX calls
-        // @see http://www.redotheweb.com/2015/11/09/api-security.html
-        credentials: 'include',
-        mode: 'cors',
-        body: JSON.stringify(payload),
-    })
-        .then((response) => handleResponse(response))
-        .then((json) => ({item: json}), (error) => ({error}));
-};
-
-export const updateFormEntityFactory = (path) => (id, jwt, payload) => {
-    const headers = {};
-
-    if (jwt) {
-        headers.Authorization = `JWT ${jwt}`;
-    }
-
-    const url = `${API_URL}/${path}/${id}/`;
-
-    return fetch(url, {
-        method: 'PATCH',
-        headers,
-        // Allows API to set http-only cookies with AJAX calls
-        // @see http://www.redotheweb.com/2015/11/09/api-security.html
-        credentials: 'include',
-        mode: 'cors',
-        body: payload,
-    })
-        .then((response) => handleResponse(response))
-        .then((json) => ({item: json}), (error) => ({error}));
-};
-
-export const createEntityFactory = (path) => (jwt, payload) => {
-    const headers = getHeaders(jwt);
-    const url = `${API_URL}/${path}/`;
-
-    let status;
-
-    return fetch(url, {
-        method: 'POST',
-        headers,
-        // Allows API to set http-only cookies with AJAX calls
-        // @see http://www.redotheweb.com/2015/11/09/api-security.html
-        credentials: 'include',
-        mode: 'cors',
-        body: JSON.stringify(payload),
-    })
-        .then((response) => {
-            status = response.status;
-            if (status !== 201) {
-                return response.text().then((result) => Promise.reject(new Error(result)));
-            }
-
-            return response.json();
-        })
-        .then((json) => ({item: json, status}), (error) => ({error, status}));
-};
-
-export const createFormEntityFactory = (path) => (jwt, payload) => {
-    const headers = {};
-
-    if (jwt) {
-        headers.Authorization = `JWT ${jwt}`;
-    }
-
-    const url = `${API_URL}/${path}/`;
-
-    let status;
-
-    return fetch(url, {
-        method: 'POST',
-        headers,
-        // Allows API to set http-only cookies with AJAX calls
-        // @see http://www.redotheweb.com/2015/11/09/api-security.html
-        credentials: 'include',
-        mode: 'cors',
-        body: payload,
-    })
-        .then((response) => {
-            status = response.status;
-            if (status !== 201) {
-                return response.text().then((result) => Promise.reject(new Error(result)));
-            }
-
-            return response.json();
         })
         .then((json) => ({item: json, status}), (error) => ({error, status}));
 };
