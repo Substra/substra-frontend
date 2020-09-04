aca962cd1890f66cf81b5737236c42d5c3f2f49f
Jérémy Morel <jeremy.morel@owkin.com>
2020-09-04 14:30:27 +0200

    remove unused code (#75)

--- src/app/utils/log.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const log = (BaseComponent) => (props) => {
-    console.log(`Rendering ${BaseComponent.name}`);
-    return <BaseComponent {...props} />;
-};
-
-export default log;
