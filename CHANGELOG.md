dadac7510862b17fcc487656988a17ad206a3217
Romain Le Quellec <13620579+romainlq@users.noreply.github.com>
2021-11-16 10:17:28 +0100

    fix: add check to avoid NaN percentage (#238)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,6 +24,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 -   Added min width to compute plans table columns to fix display when there are no results (#229)
 -   Truncate TableDrawerSectionKeyEntry value when too long (#234)
 -   Set minWidth on a compute plan tag in order to avoid to many multilines (#235)
+-   Avoid NaN progression percentage (#238)
 
 ### Changed
 
--- src/routes/computePlanDetails/components/DetailsSidebar.tsx
+++ src/routes/computePlanDetails/components/DetailsSidebar.tsx
@@ -17,9 +17,12 @@ const DetailsSidebar = (): JSX.Element => {
         (state) => state.computePlans.computePlan
     );
 
-    const percentageDisplay = computePlan
-        ? ((100 * computePlan.done_count) / computePlan.task_count).toFixed(0)
-        : 0;
+    const percentageDisplay =
+        computePlan && computePlan.task_count > 0
+            ? ((100 * computePlan.done_count) / computePlan.task_count).toFixed(
+                  0
+              )
+            : 0;
 
     return (
         <VStack spacing="8" width="xs" alignItems="stretch">
