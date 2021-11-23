a341c3593bde6ab7cd87d1bbdbc2ae39aeff6605
Romain Le Quellec <13620579+romainlq@users.noreply.github.com>
2021-11-23 14:19:28 +0100

    style: use correct Inter font (#244)
    * style: use correct Inter font
    
    * docs: add changes to Changelog

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,16 @@ import { StoreProvider } from '@/store';
 import { StrictMode } from 'react';
 
 import { ChakraProvider } from '@chakra-ui/react';
-import '@fontsource/inter';
 import ReactDOM from 'react-dom';
 
+import Fonts from '@/assets/Fonts';
 import theme from '@/assets/chakraTheme';
 
 ReactDOM.render(
     <StoreProvider>
         <StrictMode>
             <ChakraProvider theme={theme}>
+                <Fonts />
                 <App />
             </ChakraProvider>
         </StrictMode>
