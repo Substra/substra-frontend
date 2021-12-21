b2c613a5482e52d2bd2f309533c4be6f1d53fa52
Milan Roustan <milan.roustan@owkin.com>
2021-12-21 10:21:20 +0100

    fix: Change undownlodable outmodels message color (#289)
    * :lipstick: Change undownlodable outmodels message color
    
    * :memo: Update changelog

--- src/routes/tasks/components/TableDrawerSectionOutModelEntry.tsx
+++ src/routes/tasks/components/TableDrawerSectionOutModelEntry.tsx
@@ -1,4 +1,4 @@
-import { Button, VStack } from '@chakra-ui/react';
+import { Button, VStack, Text } from '@chakra-ui/react';
 import { RiDownloadLine } from 'react-icons/ri';
 
 import { getHeadModel, getSimpleModel } from '@/modules/tasks/ModelsUtils';
@@ -31,24 +31,36 @@ const TableDrawerSectionOutModelEntry = ({
     let content = null;
 
     if (task.status === TupleStatus.waiting) {
-        content = "Model training hasn't started yet";
+        content = (
+            <Text color="gray.500">Model training hasn't started yet</Text>
+        );
     } else if (task.status === TupleStatus.todo) {
-        content = "Model training hasn't started yet";
+        content = (
+            <Text color="gray.500">Model training hasn't started yet</Text>
+        );
     } else if (task.status === TupleStatus.doing) {
-        content = 'Model still training';
+        content = <Text color="gray.500">Model still training</Text>;
     } else if (task.status === TupleStatus.failed) {
-        content = 'Model training failed';
+        content = <Text color="gray.500">Model training failed</Text>;
     } else if (task.status === TupleStatus.canceled) {
-        content = 'Model training canceled';
+        content = <Text color="gray.500">Model training canceled</Text>;
     } else if (task.status === TupleStatus.done) {
         const simpleModel = getSimpleModel(task);
         const headModel = getHeadModel(task);
 
         if (simpleModel && !canDownloadModel(simpleModel.permissions)) {
-            content =
-                'Not enough permissions to see the simpleModel or missing required configuration on the server';
+            content = (
+                <Text color="gray.500">
+                    Not enough permissions to see the simpleModel or missing
+                    required configuration on the server
+                </Text>
+            );
         } else if (!simpleModel?.address?.storage_address) {
-            content = 'Intermediary simpleModel no longer available';
+            content = (
+                <Text color="gray.500">
+                    Intermediary simpleModel no longer available
+                </Text>
+            );
         } else if (
             (isTraintuple(task) || isAggregatetuple(task)) &&
             simpleModel
