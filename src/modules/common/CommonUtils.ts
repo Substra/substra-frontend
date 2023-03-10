3f9f398e1937180f4f9e0f73039c07f79df8a5f5
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-03-10 15:07:38 +0100

    fix: revert "chore: remove unused function"
    This reverts commit 7a0eb9f6c83d6e8c85e6266505e59f232ce52ba2.
    
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- src/modules/common/CommonUtils.ts
+++ src/modules/common/CommonUtils.ts
@@ -1,6 +1,8 @@
+import { AxiosPromise } from 'axios';
+
 import { capitalize as capitalizeString } from '@/libs/utils';
 
-import { AssetT } from './CommonTypes';
+import { AssetT, PaginatedApiResponseT } from './CommonTypes';
 
 const ASSET_LABEL: Record<AssetT, string> = {
     function: 'function',
@@ -22,4 +24,20 @@ export const getAssetLabel = (
         label = label + 's';
     }
     return label;
+};
+
+export const getAllPages = async <T>(
+    getPage: (page: number) => AxiosPromise<PaginatedApiResponseT<T>>,
+    pageSize: number
+): Promise<T[]> => {
+    let res: T[] = [];
+    let page = 1;
+    let lastPage = 1;
+    while (page <= lastPage) {
+        const response = await getPage(page);
+        res = [...res, ...response.data.results];
+        lastPage = Math.ceil(response.data.count / pageSize);
+        page += 1;
+    }
+    return res;
 };
