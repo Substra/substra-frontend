2ff9ffc811eb81f1d2d3bea2d61127189170eeb7
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-10-16 16:59:26 +0200

    test: update tests
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- src/routes/computePlanDetails/workflow/CPWorkflowLayout.spec.ts
+++ src/routes/computePlanDetails/workflow/CPWorkflowLayout.spec.ts
@@ -67,89 +67,6 @@ const twoTasksLayoutedGraph: LayoutedTaskGraphT = {
     edges: twoTasksGraph.edges,
 };
 
-const twoTasksPlusPredictAndTestTupleGraph: TaskGraphT = {
-    //............................
-    // pharma1
-    //         a --------------+
-    //         |               |
-    //         +-> predict_a   |
-    //             |           |
-    //             +-> test_a  |
-    //............................
-    // pharma2                 |
-    //                         +-> b
-    tasks: [
-        ...twoTasksGraph.tasks,
-        {
-            key: 'predict_a',
-            rank: 1,
-            function_name: 'Predict on MyAlgo',
-            worker: 'pharma1',
-            status: TaskStatus.done,
-            inputs_specs: [{ identifier: 'in_model', kind: 'ASSET_MODEL' }],
-            outputs_specs: [{ identifier: 'out_model', kind: 'ASSET_MODEL' }],
-        },
-        {
-            key: 'test_a',
-            rank: 2,
-            function_name: 'Testing on MyAlgo',
-            worker: 'pharma1',
-            status: TaskStatus.done,
-            inputs_specs: [{ identifier: 'in_model', kind: 'ASSET_MODEL' }],
-            outputs_specs: [{ identifier: 'perf', kind: 'ASSET_PERFORMANCE' }],
-        },
-    ],
-    edges: [
-        ...twoTasksGraph.edges,
-        {
-            source_task_key: 'a',
-            source_output_identifier: 'out_model',
-            target_task_key: 'predict_a',
-            target_input_identifier: 'in_model',
-        },
-        {
-            source_task_key: 'predict_a',
-            source_output_identifier: 'out_model',
-            target_task_key: 'test_a',
-            target_input_identifier: 'in_model',
-        },
-    ],
-};
-
-const twoTasksPlusPredictAndTestTupleLayoutedGraph: LayoutedTaskGraphT = {
-    tasks: [
-        {
-            ...twoTasksPlusPredictAndTestTupleGraph.tasks[0],
-            position: {
-                x: 0, // 0 ("in first column")
-                y: 0, // 0 ("first task in the row")
-            },
-        },
-        {
-            ...twoTasksPlusPredictAndTestTupleGraph.tasks[1],
-            position: {
-                x: 400, // 1 * CELL_WIDTH ("task in second column")
-                y: 608, // 3 * (NODE_HEIGHT+NODE_BOTTOM_MARGIN) ("3 tasks in first row = first row height") + ROW_BOTTOM_MARGIN + 0 ("first task in the row") + 8 ("uneven column top padding")
-            },
-        },
-        {
-            ...twoTasksPlusPredictAndTestTupleGraph.tasks[2],
-            position: {
-                x: 30, // 0 ("in first column") + PREDICT_TASK_LEFT_PADDING
-                y: 190, // 1 * (NODE_HEIGHT+NODE_BOTTOM_MARGIN) ("second task in the row")
-            },
-        },
-        {
-            ...twoTasksPlusPredictAndTestTupleGraph.tasks[3],
-            position: {
-                x: 60, // 0 ("in first column") + PREDICT_TASK_LEFT_PADDING
-                y: 380, // 2 * (NODE_HEIGHT+NODE_BOTTOM_MARGIN) ("thirsd task in the row")
-            },
-        },
-    ],
-    edges: twoTasksPlusPredictAndTestTupleGraph.edges,
-};
-
 const singleChainGraphWithBypassingEdge: TaskGraphT = {
     //............................
     // pharma1
@@ -240,9 +157,6 @@ const singleChainLayoutedGraphWithBypassingEdge: LayoutedTaskGraphT = {
 test('computeLayout', () => {
     expect(computeLayout(emptyGraph)).toStrictEqual(emptyGraph);
     expect(computeLayout(twoTasksGraph)).toStrictEqual(twoTasksLayoutedGraph);
-    expect(computeLayout(twoTasksPlusPredictAndTestTupleGraph)).toStrictEqual(
-        twoTasksPlusPredictAndTestTupleLayoutedGraph
-    );
     expect(computeLayout(singleChainGraphWithBypassingEdge)).toStrictEqual(
         singleChainLayoutedGraphWithBypassingEdge
     );
